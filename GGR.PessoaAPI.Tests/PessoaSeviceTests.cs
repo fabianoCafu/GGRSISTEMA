293ccc1928ef768bcea531aa2da837125936cc2b
FΛBIΛПӨ Ƨ. DΛ ƧIᄂVΛ <fabianocafu@gmail.com>
2026-01-30 07:21:26 -0300

    Ajuste - Commit arquivo PessoaAPI.Test, o mesmo tinha sido alterado para validação do CI/CD.

--- GGR.PessoaAPI.Tests/PessoaSeviceTests.cs
+++ GGR.PessoaAPI.Tests/PessoaSeviceTests.cs
@@ -43,7 +43,7 @@ namespace GGR.PessoaAPI.Tests
 
             // Assert
             Assert.True(result.IsSuccess);
-            Assert.False(result.IsSuccess);
+            Assert.False(result.IsFailure);
         }
 
         [Fact]
